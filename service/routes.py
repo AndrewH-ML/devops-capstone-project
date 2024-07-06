52e09fb9e297529fc86dfd2c7f0535f38ea2a058
Andrew <40874538+AndrewH-ML@users.noreply.github.com>
2024-07-06 04:36:20 -0400

    Wrote tests cases for updating an account & tests to succesfully pass those tests

--- service/routes.py
+++ service/routes.py
@@ -88,6 +88,19 @@ def read_account(account_id):
 
 # ... place you code here to UPDATE an account ...
 
+@app.route("/accounts/<int:account_id>", methods=["PUT"])
+def update_account(account_id):
+    """
+    Updates an account
+    This endpoint will update an account by requested account id"""
+    app.logger.info("Request to update an account with id: %s", account_id)
+
+    account = Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+    account.deserialize(request.get_json())
+    account.update()
+    return account.serialize(), status.HTTP_200_OK
 
 ######################################################################
 # DELETE AN ACCOUNT
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -138,3 +138,24 @@ class TestAccountService(TestCase):
             f"{BASE_URL}/{0}", content_type="application/json"
         )
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    
+    def test_update_account(self):
+        """It should update an Account"""
+        test_account = AccountFactory()
+        response = self.client.post(BASE_URL, json=test_account.serialize())
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
+        new_account = response.get_json()
+        new_account["name"] = "test_name"
+        
+        response = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        updated_account = response.get_json()
+        self.assertEqual(updated_account["name"], "test_name")
+
+    def test_update_nonexistent_account(self):
+        """It should fail update and return 404 NOT FOUND"""
+        test_account = AccountFactory()
+        test_account.name = "test_name"
+        response = self.client.put(f"{BASE_URL}/{test_account.id}", json=test_account.serialize())
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
